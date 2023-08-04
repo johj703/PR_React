0781657a1f50e990c11723db13acb44c432dafe8
Jo <johj703@nate.com>
2023-08-04 19:41:45 +0900

    useEffect 작성

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");
+  console.log("I run all the time");
+  // useEffect 함수는 코드가 딱 한 번만 실행 될 수 있도록 보호해 준다
+  useEffect(() => {
+    console.log("call the API");
+  }, []);
   // State를 변경할 때, 모든 코드들은 항상 다시 실행된다
   return (
     <div>
