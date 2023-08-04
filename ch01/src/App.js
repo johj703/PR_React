57089d0ad2f7eebc948d0376a16a774fdc2dc69a
Jo <johj703@nate.com>
2023-08-04 17:04:27 +0900

    Update App.js

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log("call an api");
+  // State를 변경할 때, 모든 코드들은 항상 다시 실행된다
   return (
     <div>
       <h1>{counter}</h1>
