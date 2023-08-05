788ea6b4bf3bf98465ca9cec730df0d6071f2ad3
Jo <johj703@nate.com>
2023-08-05 12:37:45 +0900

    useState 생성, onChange 함수 작성 및 onChange 적용

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -2,7 +2,9 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("I run all the time");
   // useEffect 함수는 코드가 딱 한 번만 실행 될 수 있도록 보호해 준다
   useEffect(() => {
@@ -11,7 +13,7 @@ function App() {
   // State를 변경할 때, 모든 코드들은 항상 다시 실행된다
   return (
     <div>
-      <input type="text" placeholder="Search here" />
+      <input onChange={onChange} type="text" placeholder="Search here" />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
