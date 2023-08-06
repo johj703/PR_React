d459437c7dd040e41670b3bcd78ad60c3588dd18
Jo <johj703@nate.com>
2023-08-06 18:29:19 +0900

    Cleanup 세팅

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,36 +1,8 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  // useEffect 함수는 코드가 딱 한 번만 실행 될 수 있도록 보호해 준다
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter change");
-  }, [keyword, counter]);
-  // State를 변경할 때, 모든 코드들은 항상 다시 실행된다
-  return (
-    <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
-  );
+  return;
+  <div></div>;
 }
 
 export default App;
