cc6103f3cc74b1c77cd1c8a9a9c55f7b7a607558
Jo <johj703@nate.com>
2023-08-07 13:03:15 +0900

    입력창에 글씨를 넣으면 콘솔에 나타남

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  console.log(toDo);
+  return (
+    <div>
+      <input
+        onChange={onChange}
+        value={toDo}
+        type="text"
+        placeholder="Write your to do!"
+      />
+    </div>
+  );
 }
 
 export default App;
