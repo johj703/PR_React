7aa8e34e517c7ec869a843e76c33539bc37eae89
Jo <johj703@nate.com>
2023-08-07 12:01:27 +0900

    To Do List 만들기 세팅

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,28 +1,5 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return (
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
