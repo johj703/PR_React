930bf838962f02a5bd4e2ab729b6f978e341bb02
Jo <johj703@nate.com>
2023-08-08 21:28:58 +0900

    Add To Do 버튼과 기능 만들기

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -3,15 +3,22 @@ import { useState } from "react";
 function App() {
   const [toDo, setToDo] = useState("");
   const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+  };
   console.log(toDo);
   return (
     <div>
-      <input
-        onChange={onChange}
-        value={toDo}
-        type="text"
-        placeholder="Write your to do!"
-      />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do!"
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
