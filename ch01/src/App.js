52482e856d279c039f43c1a813fc0cb215c8bd44
Jo <johj703@nate.com>
2023-08-10 18:06:53 +0900

    tickers API 받아서 콘솔에 보이게 작성

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
   return (
     <div>
       <h1>The Coins!!</h1>
