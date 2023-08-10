121d36b9d099609896d50d43586d0ce821c93b7b
Jo <johj703@nate.com>
2023-08-10 17:19:01 +0900

    text 출력 확인

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,5 +1,13 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  return (
+    <div>
+      <h1>The Coins!!</h1>
+      {loading ? <strong>Loading...</strong> : null}
+    </div>
+  );
 }
 
 export default App;
