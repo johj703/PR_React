7282d95d9fc9d20c62195857a9c012bd105cbe8e
Jo <johj703@nate.com>
2023-08-12 18:23:14 +0900

    브라우저 콘솔에서 object를 받아온 것을 확인 가능

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    )
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
