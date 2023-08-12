d629d922383e3e8b65c64ab2241ca99fe9121e47
Jo <johj703@nate.com>
2023-08-12 18:37:15 +0900

    Loading을 끝냈기 때문에 setLoading을 false로 만들어야 한다.

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -8,7 +8,10 @@ function App() {
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
     )
       .then((response) => response.json())
-      .then((json) => setMovies(json.data.movies));
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
   }, []);
   console.log(movies);
   return <div>{loading ? <h1>Loading...</h1> : null}</div>;
