bb42f985e7ef15a517ed73d40b8b7780e300ea68
Jo <johj703@nate.com>
2023-08-13 19:51:46 +0900

    Update Movie.js

--- ch01/src/Movie.js
+++ ch01/src/Movie.js
@@ -1,7 +1,7 @@
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
-      <img src={coverImg} />
+      <img src={coverImg} alt={title} />
       <h2>{title}</h2>
       <p>{summary}</p>
       <ul>
