964034e60d1f0c8e83e97ee4176ba93b7231e956
Jo <johj703@nate.com>
2023-08-14 16:38:04 +0900

    Router로 등록하기

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
