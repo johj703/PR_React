4e3a6386aee164f5bc5a05cdda73469da9c1b9cc
Jo <johj703@nate.com>
2023-08-14 16:50:56 +0900

    Detail 컴포넌트 등록

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
       </Routes>
     </Router>
   );
